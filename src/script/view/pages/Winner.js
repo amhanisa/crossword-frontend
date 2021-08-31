01f86793ae2e22a4f0551bb09eed7f73fb7dca38
amhanisa <azka@amhanisa.com>
2021-08-31 19:08:02 +0700

    limit winner to 5 + time

--- src/script/view/pages/Winner.js
+++ src/script/view/pages/Winner.js
@@ -4,7 +4,8 @@ import CONFIG from "../../data/config";
 const Winner = {
   async render() {
     return `
-    <div class="setting winner-setting">
+    <div class="setting">
+      <div class="winner-setting">
         <ol id="score-list">
         </ol>
         <div>
@@ -14,11 +15,6 @@ const Winner = {
               <input id="winner3">
               <input id="winner4">
               <input id="winner5">
-              <input id="winner6">
-              <input id="winner7">
-              <input id="winner8">
-              <input id="winner9">
-              <input id="winner10">
             </div>
             <div class="flex">
               <input id="score1">
@@ -26,15 +22,23 @@ const Winner = {
               <input id="score3">
               <input id="score4">
               <input id="score5">
-              <input id="score6">
-              <input id="score7">
-              <input id="score8">
-              <input id="score9">
-              <input id="score10">
+            </div>
+            <div class="flex">
+              <input id="time1">
+              <input id="time2">
+              <input id="time3">
+              <input id="time4">
+              <input id="time5">
             </div>
             <button id="submit-winner">Submit Winner</button>
         </div>
-
+        <div class="time-setting">
+        <input type="date" id="date-selector" class="date">
+        <input type="time" id="time-selector" class="date">
+        <button id="set-time">Apply</button>
+        <p id="selected-time"></p>        
+      </div>
+      </div>
     </div>
      
     <button id="toggle-setting">Toggle Setting</button>
@@ -53,6 +57,9 @@ const Winner = {
             <td>
             Score
             </td>
+            <td>
+            Time
+            </td>
         </tr>
         </thead>  
         <tbody></tbody>
@@ -61,23 +68,43 @@ const Winner = {
   },
 
   async afterRender() {
-    axios.get(`${CONFIG.API_URL}/winner`).then((res) => {
-      const data = res.data;
-      const scoreList = document.querySelector("#score-list");
-      console.log(data);
-
-      data.forEach((score) => {
-        scoreList.innerHTML += `
-            <li>
-              ${score.username} | ${score.score}
-            </li>
-        `;
-      });
-    });
-
     const settingButton = document.querySelector("#toggle-setting");
     settingButton.addEventListener("click", () => {
       document.querySelector(".setting").classList.toggle("hide");
+    });
+
+    const setTimeButton = document.querySelector("#set-time");
+    setTimeButton.addEventListener("click", () => {
+      const date = document.querySelector("#date-selector").value;
+      const time = document.querySelector("#time-selector").value;
+
+      document.querySelector("#selected-time").innerHTML = new Date(
+        date + " " + time
+      );
+
+      const startDate = new Date(
+        document.querySelector("#selected-time").innerHTML
+      );
+
+      axios.get(`${CONFIG.API_URL}/winner`).then((res) => {
+        const data = res.data;
+        const scoreList = document.querySelector("#score-list");
+        console.log(data);
+        scoreList.innerHTML = "";
+
+        data.forEach((score) => {
+          const endDate = new Date(score.time);
+          const duration = (endDate.getTime() - startDate.getTime()) / 1000;
+          const minute = Math.floor(duration / 60);
+          const second = duration % 60;
+
+          scoreList.innerHTML += `
+              <li>
+                ${score.username} | ${score.score} | ${minute} Menit ${second} Detik
+              </li>
+          `;
+        });
+      });
     });
 
     const submitWinner = document.querySelector("#submit-winner");
@@ -85,9 +112,10 @@ const Winner = {
       const table = document.querySelector("tbody");
       table.innerHTML = "";
 
-      for (let i = 1; i <= 10; i++) {
+      for (let i = 1; i <= 5; i++) {
         const username = document.querySelector(`#winner${i}`).value;
         const score = document.querySelector(`#score${i}`).value;
+        const time = document.querySelector(`#time${i}`).value;
         console.log(username);
         console.log(score);
         const row = `
@@ -95,6 +123,7 @@ const Winner = {
             <td>${i}</td>
             <td class="truncate text-bold">${username}</td>
             <td>${score}</td>
+            <td>${time}</td>
           </tr>
         `;
 
